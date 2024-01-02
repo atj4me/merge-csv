a42bbb55f42995340bb8c424013c40699ff785b8
Ajith Thampi Joseph <atj4me@hotmail.com>
2024-01-02 02:17:50 -0500

    Debugging

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ while IFS= read -r scriptId
 do
   # Set the script ID in .clasp.json
   echo "{\"scriptId\":\"$scriptId\", \"rootDir\":\"src\"}" > .clasp.json
-
+  echo "Working with $scriptId"
   # Push the code to the script
   npx clasp push
 done < script-ids.txt
